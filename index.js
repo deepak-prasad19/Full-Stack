74bc69961d2c29a4ec6d4704480eb87596f3d689
unknown <deepak191225@gmail.com>
2024-06-12 11:56:06 +0530

    Initial commit of SQL_with_FrontEnd project

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const mysql = require("mysql2");
+const { faker } = require("@faker-js/faker");
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const connection = mysql.createConnection({
+    host: "localhost", 
+    user: "root", 
+    password: "Deepak@1999",
+    database: "temp",
+});
+
+let getRandomData = () => {
+    return [
+        faker.datatype.uuid(),
+        faker.internet.userName(),
+        faker.internet.email(),
+        faker.internet.password(),
+    ];
+};
+
+app.get("/", (req, res) => {
+    let q = "SELECT COUNT(*) FROM company";
+
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            console.log(result);
+            res.render("index.ejs", { result[0].count });
+        });
+    }catch(err) {
+        console.log("Some errors occured", err);
+    }
+});
+
+app.listen(port, (req, res) => {
+    console.log("Port is open for request's");
+});
+
+// const q = "insert into company (id, username, email, password) values ?";
+// let data = [];
+
+// for(let i=0; i<100; i++) {
+//     data.push(getRandomData());
+// }
+
+// try {
+//     connection.query(q, [data], (err, result) => {
+//         if (err) throw err;
+//         console.log(result);
+//     });
+// }catch(err) {
+//     console.log(err);
+// }
+// finally {
+//     connection.end();
+// }
---
+++ schema.sql
@@ -0,0 +1,8 @@
+/*CREATE TABLE company (
+    id varchar(50) primary key,
+    username varchar(50) unique,
+    email varchar(50) unique not null,
+    password varchar(50) not null
+);*/
+
+SELECT * FROM company;
